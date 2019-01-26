37173e8c9e4f48b88ba34418b471a999b745c8cc
DinoZunic04 <46324462+DinoZunic04@users.noreply.github.com>
2019-01-26 16:17:36 +0100

    Update Kvadratna jednacina.c

--- Kvadratna jednacina.c
+++ Kvadratna jednacina.c
@@ -39,12 +39,12 @@ int main()
             xR=-b/(2*a);
             xI=sqrt(4*a*c-b*b)/(2*a);
             if(b!=0.0)
-                {
-                    if(xI!=1)
-                        printf("x = %.2f + %.2f*i ili x = %.2f - %.2f*i.\n",xR,xI,xR,xI);
-                    else
-                        printf("x = %.2f + i ili x = %.2f - i.\n",xR,xR);
-                }
+            {
+                if(xI!=1)
+                    printf("x = %.2f + %.2f*i ili x = %.2f - %.2f*i.\n",xR,xI,xR,xI);
+                else
+                    printf("x = %.2f + i ili x = %.2f - i.\n",xR,xR);
+            }
             else
             {
                 if(xI!=1)
